e3e66dcb277f5a33e75effd266b176aeb9e07ffc
Zachary <zachness2003@gmail.com>
2024-11-11 08:21:05 -0600

    Added new map

--- Scripts/CometMan.gd
+++ Scripts/CometMan.gd
@@ -13,13 +13,13 @@ func _ready():
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("up"):
-		direction = Vector3(0, 0, -1)
-	elif Input.is_action_just_pressed("down"):
 		direction = Vector3(0, 0, 1)
+	elif Input.is_action_just_pressed("down"):
+		direction = Vector3(0, 0, -1)
 	elif Input.is_action_just_pressed("right"):
-		direction = Vector3(1, 0, 0)
-	elif Input.is_action_just_pressed("left"):
 		direction = Vector3(-1, 0, 0)
+	elif Input.is_action_just_pressed("left"):
+		direction = Vector3(1, 0, 0)
 	
 	velocity = direction * movement_speed
 	move_and_slide()
